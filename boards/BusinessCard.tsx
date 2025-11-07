f6ae33a0deaec8bade53945bd92e3718612808a6
seveibar <seveibar@gmail.com>
2025-11-07 15:08:00 -0800

    new business-card-sized design

--- boards/BusinessCard.tsx
+++ boards/BusinessCard.tsx
@@ -12,46 +12,46 @@ const Via = (props: { pcbX: number | string; pcbY: number | string }) => (
   />
 )
 
-const VIA_SPACING = 1.2
-const ViaRow = (props: {
-  viaCount: number
-  startX: number
-  startY: number
-  shouldSkip?: (index: number) => boolean
-  direction: "x+" | "x-" | "y+" | "y-"
-  spacing?: number
-}) => (
-  <>
-    {Array.from({ length: props.viaCount })
-      .map((_, index) =>
-        props.shouldSkip ? (props.shouldSkip(index) ? null : index) : index,
-      )
-      .filter((index) => index !== null)
-      .map((index) =>
-        props.direction === "x+" ? (
+const range = (start: number, end: number, inc = 1) => {
+  return Array.from({ length: (end - start) / inc }, (_, i) => start + i * inc)
+}
+
+const ViaZone = (props: {
+  minX: number
+  maxX: number
+  minY: number
+  maxY: number
+  spacing: number
+}) => {
+  const xRange = range(props.minX, props.maxX, props.spacing)
+  const yRange = range(props.minY, props.maxY, props.spacing)
+  const centeringOffsetX =
+    (props.maxX - props.minX - (xRange.length - 1) * props.spacing) / 2
+  const centeringOffsetY =
+    (props.maxY - props.minY - (yRange.length - 1) * props.spacing) / 2
+
+  return (
+    <>
+      <pcbnoterect
+        color="blue"
+        width={props.maxX - props.minX}
+        height={props.maxY - props.minY}
+        pcbPositionAnchor="center"
+        pcbX={props.minX + (props.maxX - props.minX) / 2}
+        pcbY={props.minY + (props.maxY - props.minY) / 2}
+      />
+      {xRange.flatMap((x) =>
+        yRange.map((y) => (
           <Via
-            pcbX={props.startX + index * (props.spacing ?? VIA_SPACING)}
-            pcbY={props.startY}
+            key={`${x},${y}`}
+            pcbX={x + centeringOffsetX}
+            pcbY={y + centeringOffsetY}
           />
-        ) : props.direction === "x-" ? (
-          <Via
-            pcbX={props.startX - index * (props.spacing ?? VIA_SPACING)}
-            pcbY={props.startY}
-          />
-        ) : props.direction === "y+" ? (
-          <Via
-            pcbX={props.startX}
-            pcbY={props.startY + index * (props.spacing ?? VIA_SPACING)}
-          />
-        ) : (
-          <Via
-            pcbX={props.startX}
-            pcbY={props.startY - index * (props.spacing ?? VIA_SPACING)}
-          />
-        ),
+        )),
       )}
-  </>
-)
+    </>
+  )
+}
 
 const boardWidthMm = 85
 const boardHeightMm = 55
@@ -62,13 +62,12 @@ export default () => (
     borderRadius="2mm"
   >
     {/* <chip footprint="qfp80" name="U1" pcbX={-6} /> */}
-    <chip
+    {/* <chip
       footprint={<XiaoBoardFootprint variant="Receiver" />}
       name="U1"
       pcbX={-6}
-      pcbY={-10}
-    />
-    {/* <XiaoBoard name="U1" variant="RP2040" /> */}
+      pcbY={-18}
+    /> */}
     <net name="GND" />
     <copperpour
       connectsTo="net.GND"
@@ -92,71 +91,34 @@ export default () => (
       to={{ x: boardWidthMm / 2 + 2.5, y: boardHeightMm / 2 }}
       text={`${boardHeightMm}mm`}
     />
-    {[-23, -21, 7, 10].map((x) => (
-      <>
-        <ViaRow
-          shouldSkip={(index) => (Math.abs(x) === 21 ? index % 2 === 0 : false)}
-          viaCount={4}
-          startX={x}
-          startY={18}
-          direction="y-"
-        />
-        <ViaRow
-          viaCount={4}
-          shouldSkip={(index) => (Math.abs(x) === 21 ? index % 2 === 0 : false)}
-          startX={x}
-          startY={12}
-          direction="y-"
-        />
-        <ViaRow
-          shouldSkip={(index) => (Math.abs(x) === 21 ? index % 2 === 0 : false)}
-          viaCount={4}
-          startX={x}
-          startY={0}
-          direction="y-"
-        />
-        <ViaRow
-          shouldSkip={(index) => (Math.abs(x) === 21 ? index % 2 === 0 : false)}
-          viaCount={4}
-          startX={x}
-          startY={-8}
-          direction="y-"
-        />
-        <ViaRow
-          shouldSkip={(index) => (Math.abs(x) === 21 ? index % 2 === 0 : false)}
-          viaCount={2}
-          startX={x}
-          startY={-16}
-          direction="y-"
-        />
-      </>
-    ))}
-    <ViaRow viaCount={3} startX={0} startY={-17} direction="y+" />
-    <ViaRow viaCount={9} startX={22.5} startY={18} direction="y-" spacing={2} />
-    <ViaRow viaCount={9} startX={0} startY={17} direction="x-" spacing={2} />
-    <ViaRow viaCount={4} startX={-13} startY={-11} spacing={2} direction="x+" />
-    {/* <TYPE_C_16P_QTWT
-			name="J1"
-			pcbX={-11}
-			pcbY={-16}
-			connections={{
-				pin1: "net.GND",
-				pin2: "net.GND",
-				pin3: "net.GND",
-				pin4: "net.GND",
-				pin5: "net.GND",
-				pin6: "net.GND",
-				pin7: "net.GND",
-				pin8: "net.GND",
-				pin9: "net.GND",
-				pin10: "net.GND",
-				pin11: "net.GND",
-				pin12: "net.GND",
-				pin13: "net.GND",
-				pin14: "net.GND",
-				pin15: "net.GND",
-				pin16: "net.GND",
-			}}
-		/> */}
+    <ViaZone
+      minX={-boardWidthMm / 2 + 2}
+      maxX={-boardWidthMm / 2 + 10}
+      minY={-boardHeightMm / 2 + 2}
+      maxY={-boardHeightMm / 2 + 10}
+      spacing={2}
+    />
+    <ViaZone
+      minX={-boardWidthMm / 2 + 2}
+      maxX={-boardWidthMm / 2 + 10}
+      minY={boardHeightMm / 2 - 10}
+      maxY={boardHeightMm / 2 - 2}
+      spacing={2}
+    />
+    <ViaZone
+      minX={10}
+      maxX={boardWidthMm / 2 - 10}
+      minY={boardHeightMm / 2 - 10}
+      maxY={boardHeightMm / 2 - 2}
+      spacing={3}
+    />
+    <ViaZone
+      minX={boardWidthMm / 2 - 5 - 2}
+      maxX={boardWidthMm / 2 - 5 + 2}
+      minY={-boardHeightMm / 2 + 5}
+      maxY={boardHeightMm / 2 - 5}
+      spacing={2}
+    />
+    <ViaZone minX={-20} maxX={0} minY={-8} maxY={8} spacing={4} />
   </board>
 )
