c18ff97ba99542caeb14302c58888d46f7a33544
seveibar <seveibar@gmail.com>
2026-08-13 13:57:49 -0700

    add bare clad circuit entrypoints

---
+++ examples/biscuit-board.circuit.tsx
@@ -0,0 +1,4 @@
+import { BiscuitBoard } from "../lib/BiscuitBoard"
+
+/** Bare biscuit-board clad preview: mechanics and candidate vias only. */
+export default () => <BiscuitBoard routingDisabled />
