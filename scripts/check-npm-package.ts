3910742e82ec199283af097d668b96b20ba36407
seveibar <seveibar@gmail.com>
2026-08-18 13:42:23 -0700

    export Clad32x32 from npm entrypoint

--- scripts/check-npm-package.ts
+++ scripts/check-npm-package.ts
@@ -6,6 +6,7 @@ const expectedExports = [
   "BiscuitBoard",
   "BoosterPackClad",
   "BreadboardClad",
+  "Clad32x32",
   "Clad40x40",
   "FeatherCladWithPinHeaders",
   "XiaoCladWithPerforatedPinHeaders",
